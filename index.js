f24dfad3ee8b8c49c745b5d39642f68aeff55010
Victor Muturi <muturivictor21@gmail.com>
2023-10-05 12:41:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        // Access the new id and append it to the DOM
+        document.body.innerHTML += data.id;
+      })
+      .catch(error => {
+        // Append error message to the DOM
+        document.body.innerHTML += error.message;
+      });
+  }
+  
+  // Return the fetch chain for other functions to use
+  return submitData();
+  
